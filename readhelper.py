8fc593ba98f74c354eecf3c343346595d6473a79
徐博宇 <xuboyu@xuboyudeMacBook-Pro-2.local>
2024-03-08 01:07:42 -0500

    imgreader

--- readhelper.py
+++ readhelper.py
@@ -1,11 +1,13 @@
 import glob
 import SimpleITK as sitk
 import numpy as np
+
+
 import matplotlib.pyplot as plt
 def read_img(img_path):
     return sitk.GetArrayFromImage(sitk.ReadImage(img_path))
 
 t1 = glob.glob("./BraTS_segmentation/*/*t1.nii.gz")
-img = (read_img(t1[0])[100]).astype(np.uint8)
-plt.imshow(img)
+img1 = (read_img(t1[0])[100]).astype(np.uint8)
+plt.imshow(img1)
 plt.show(block=False)
